fdbff98c503e9f346b1a9c315d3e2ced0918085f
Balex7777 <morevbalex777@gmail.com>
2025-02-26 00:07:31 +0300

    fix: alert server error

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,59 +1,55 @@
 <template>
-<div class="d-flex justify-center">
-        <v-card width="600px" class="mt-12 pa-10">
-            <v-card-title>
-                Войдите в аккаунт
-            </v-card-title>
+  <div class="d-flex justify-center">
+    <v-card width="600px" class="mt-12 pa-10">
+      <v-card-title> Войдите в аккаунт </v-card-title>
 
-            <v-text-field
-                label="Введите логин"
-                v-model="login"
-                outlined
-            ></v-text-field>
+      <v-text-field label="Введите логин" v-model="login" outlined></v-text-field>
 
-            <v-text-field
-                label="Введите пароль"
-                v-model="password"
-                outlined
-            ></v-text-field>
+      <v-text-field label="Введите пароль" v-model="password" outlined></v-text-field>
 
-            <v-btn @click="authenticate">
-                Войти
-            </v-btn>
-        </v-card>
-    </div>
+      <v-btn @click="authenticate"> Войти </v-btn>
+    </v-card>
+  </div>
 </template>
 
 <script>
+import axios from "axios"; // Импорт Axios
+
 export default {
   name: "Login",
   data() {
     return {
+      login: "",
+      password: "",
     };
   },
   methods: {
-    authenticate() {
-      this.axios
-        .get(
+    async authenticate() {
+      try {
+        const response = await axios.get(
           `https://37.77.104.246/api/jsonstorage/?id=69f756530dfb7a35cb40936791601741`
-        )
-        .then((response) => {
-          let users = response.data;
-          let found = false;
-          for(let index in users){
-              if(this.login == users[index].login && this.password == users[index].password){
-                  this.$router.push('user/' + users[index].id);
-                  this.$store.state.userData.id = users[index].id;
-                  found = true;
-                  break;
-              }
+        );
+
+        let users = response.data;
+        let found = false;
+
+        for (let user of users) {
+          if (this.login === user.login && this.password === user.password) {
+            this.$router.push(`/user/${user.id}`);
+            this.$store.state.userData.id = user.id;
+            found = true;
+            break;
           }
-          if(!found){
-              window.alert('неверный логин или пароль');
-          }
-        });
+        }
+
+        if (!found) {
+          window.alert("Неверный логин или пароль");
+        }
+      } catch (error) {
+        console.error("Ошибка при запросе данных:", error);
+        window.alert("Ошибка сервера. Попробуйте позже.");
+      }
     },
-  }
-  
+  },
 };
 </script>
